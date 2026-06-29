4efd220b39712c64599f410d1b0bf3f8b552c726
giathieu110406 <giathieu110406@gmail.com>
2026-06-29 16:02:32 +0700

    Delete test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-const check = async () => {
-  const res = await fetch('https://texlive.net/cgi-bin/latexcgi', { method: 'POST', redirect: 'manual' });
-  console.log(res.status, res.headers.get('location'));
-}
-check();
